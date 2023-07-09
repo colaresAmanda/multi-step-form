64bb89d56cabff3169d480372de59f6f675d1ea0
colaresAmanda <colares.amd@gmail.com>
2023-07-08 21:35:26 -0300

    all done

---
+++ README.md
@@ -0,0 +1,44 @@
+Registration Form
+
+This is a registration form with a multi-step wizard-like interface. It allows users to enter their account information in a step-by-step process. The form is styled using CSS and JavaScript is used to handle the form navigation.
+Features
+
+    Step-by-step form navigation
+    Responsive design for different device sizes
+    Customizable form styles using SCSS variables
+
+Technologies Used
+
+    HTML
+    CSS/SCSS
+    JavaScript
+
+Getting Started
+
+    Clone the repository to your local machine or download the source code.
+    Open the index.html file in a web browser.
+    Fill out the form fields and navigate through the steps using the "Next" and "Previous" buttons.
+
+Customization
+
+To customize the form styles, you can modify the SCSS variables defined in the partials/variables.scss file. Update the variable values according to your desired styling preferences.
+Compatibility
+
+The form is designed to work in modern web browsers. It is recommended to use the latest versions of browsers like Google Chrome, Mozilla Firefox, or Microsoft Edge for the best experience.
+License
+
+This project is licensed under the MIT License.
+Acknowledgements
+
+The design and implementation of this registration form were inspired by various form wizard implementations and design patterns found on the web.
+Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, feel free to open an issue or submit a pull request.
+Authors
+
+    Your Name - Initial work
+
+Feel free to update the author section with your information if you make modifications to the project.
+Conclusion
+
+This registration form provides a user-friendly way to collect information from users in a step-by-step manner. It can be easily customized and integrated into web projects. Feel free to use this form as a starting point for your own projects or modify it to suit your specific needs.
---
+++ script.js
@@ -0,0 +1,82 @@
+function init() {
+    // Step tracking object
+    const step = {
+      I: true,
+      II: false,
+      III: false
+    };
+  
+    // Button and element references
+    const nextButton = document.querySelector('#next-button');
+    const previousButton = document.querySelector('#previous-button');
+    const submitButton = document.querySelector('#submit-button');
+    const fieldsetElements = Array.from(document.querySelectorAll('fieldset'));
+    const formStepI = document.querySelector('#ciruclar-step-I');
+    const formStepII = document.querySelector('#ciruclar-step-II');
+    const formStepIII = document.querySelector('#ciruclar-step-III');
+  
+    // Initial values
+    let translateValue = 0;
+  
+    // Event listener for "Next" button
+    nextButton.addEventListener('click', () => {
+      if (step.I) {
+        // Toggle visibility and active state
+        previousButton.classList.toggle("hidden");
+        formStepI.classList.toggle('active');
+        formStepII.classList.toggle('active');
+        // Update translate value and step tracking
+        translateValue = 0;
+        step.I = false;
+        step.II = true;
+      } else if (step.II) {
+        // Toggle visibility and active state
+        nextButton.classList.toggle("hidden");
+        submitButton.classList.toggle("hidden");
+        formStepII.classList.toggle('active');
+        formStepIII.classList.toggle('active');
+        // Update translate value and step tracking
+        translateValue = -25;
+        step.II = false;
+        step.III = true;
+      }
+  
+      // Apply translate value to fieldset elements
+      fieldsetElements.forEach((fieldset) => {
+        fieldset.style.transform = `translateX(${translateValue}rem)`;
+      });
+    });
+  
+    // Event listener for "Previous" button
+    previousButton.addEventListener('click', () => {
+      if (step.II) {
+        // Toggle visibility and active state
+        formStepII.classList.toggle('active');
+        formStepI.classList.toggle('active');
+        // Update translate value and step tracking
+        translateValue = 25;
+        previousButton.classList.toggle("hidden");
+        step.II = false;
+        step.I = true;
+      } else if (step.III) {
+        // Toggle visibility and active state
+        formStepIII.classList.toggle('active');
+        formStepII.classList.toggle('active');
+        nextButton.classList.toggle("hidden");
+        submitButton.classList.toggle("hidden");
+        // Update translate value and step tracking
+        translateValue = 0;
+        step.III = false;
+        step.II = true;
+      }
+  
+      // Apply translate value to fieldset elements
+      fieldsetElements.forEach((fieldset) => {
+        fieldset.style.transform = `translateX(${translateValue}rem)`;
+      });
+    });
+  }
+  
+  // Initialize the form behavior
+  init();
+  
